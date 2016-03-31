1bdf6e480361adb8324f97a2ce0fac794c26cae6
Guillermo Rodriguez <guillermo@altenwald.com>
2016-03-31 15:36:09 +0200

    first commit

---
+++ src/densho_app.erl
@@ -0,0 +1,16 @@
+-module(densho_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    densho_sup:start_link().
+
+stop(_State) ->
+    ok.
